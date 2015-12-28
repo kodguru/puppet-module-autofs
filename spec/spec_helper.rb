b535d96fe07afda30f1dca61dae6663bf7a68916
Garrett Honeycutt <code@garretthoneycutt.com>
2015-12-28 15:56:16 -0500

    Test future parser by setting environment variable that Puppet uses

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,4 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |config|
   config.hiera_config = 'spec/fixtures/hiera/hiera.yaml'
-  config.before :each do
-    Puppet[:parser] = 'future' if ENV['PARSER'] == 'future'
-  end
 end
