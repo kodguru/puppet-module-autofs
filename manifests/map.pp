e16341663407ae2ac9a98fa880746265b5eca25e
Phil Friderici <phil.friderici@ericsson.com>
2017-05-04 10:50:47 +0200

    Satisfy puppet-lint

--- manifests/map.pp
+++ manifests/map.pp
@@ -18,20 +18,20 @@ define autofs::map (
     if $file == undef {
       file { "mountmap_${mnt}":
         ensure  => file,
-      	path    => $mountmap,
-      	owner   => 'root',
-      	group   => 'root',
-      	mode    => '0644',
-      	content => template('autofs/mountmap.erb'),
-    	}
+        path    => $mountmap,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template('autofs/mountmap.erb'),
+      }
     } else {
       file { "mountmap_${mnt}":
-      	ensure => file,
-      	path   => $mountmap,
-      	owner  => 'root',
-      	group  => 'root',
-      	mode   => '0644',
-      	source => $file,
+        ensure => file,
+        path   => $mountmap,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        source => $file,
       }
     }
   }
